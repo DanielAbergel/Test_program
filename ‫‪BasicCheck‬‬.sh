574d298e52c239b67237fa84b7065ae76ae64be6
DanielAbergel <danielabergel1@gmail.com>
2019-03-11 10:30:20 +0200

    update BasicCheck.sh , rm the output file

--- ‫‪BasicCheck‬‬.sh
+++ ‫‪BasicCheck‬‬.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 folderName=$1 # foler path
-executeble=$2 # the makefile output file name (a.out for example). 
-arguments=$3 # arguments for the program . to run this program with arguments do "123 123 123" for example 
+executeble=$2 # the makefile output file name (a.out for example).
+arguments=$3 # arguments for the program . 
 
 currentLocation=`pwd`
 
@@ -13,6 +13,8 @@ secssesfullMake=$? # Checks whether the makefile has succeeded
 if [[ "$secssesfullMake" -gt 0 ]]; then
 echo -e '  Compilation    leaks‬‬ Memory      thread race' 
 echo -e '     FAIL            FAIL              FAIL  ' 
+rm output.txt
+cd $currentLocation
 exit 7
 else 
 Compile="PASS"
@@ -40,4 +42,6 @@ echo -e '  Compilation    leaks‬‬ Memory      thread race'
 echo    "     $Compile            $valgrindVar              $threadrace" #program out put
 
 exitnumber="$((2#$secssesfullMake$valgrind$helgrind))" # https://unix.stackexchange.com/questions/65280/binary-to-hexadecimal-and-decimal-in-a-shell-script Binary2Decimal
+rm output.txt
+cd $currentLocation
 exit "$exitnumber"
