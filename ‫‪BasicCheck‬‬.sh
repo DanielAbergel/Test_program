fa58e558802820d1d03758b5146eb565f706333a
DanielAbergel <danielabergel1@gmail.com>
2019-03-12 17:53:36 +0200

    last update BasicCheck.sh script,add /dev/null output and an input to program option

--- ‫‪BasicCheck‬‬.sh
+++ ‫‪BasicCheck‬‬.sh
@@ -2,25 +2,24 @@
 
 folderName=$1 # foler path
 executeble=$2 # the makefile output file name (a.out for example).
-arguments=$3 # arguments for the program . 
+
 
 currentLocation=`pwd`
 
 cd "$folderName" # go into the folder path
 
-make &> output.txt
+make &> /dev/null
 secssesfullMake=$? # Checks whether the makefile has succeeded
 if [[ "$secssesfullMake" -gt 0 ]]; then
 echo -e '  Compilation    leaks‬‬ Memory      thread race' 
 echo -e '     FAIL            FAIL              FAIL  ' 
-rm output.txt
 cd $currentLocation
 exit 7
 else 
 Compile="PASS"
 fi
 
-valgrind --leak-check=full --error-exitcode=1  ./"$executeble" $arguments &> output.txt
+valgrind --leak-check=full --error-exitcode=1  ./"$executeble" shift 2 "$@" &> /dev/null
 valgrind=$? # Checks whether the valgrind(memo check) has succeeded
 if [[ "$valgrind" -gt 0 ]]; then
 valgrindVar="FAIL"
@@ -29,7 +28,7 @@ else
 valgrindVar="PASS"
 fi
 
-valgrind --tool=helgrind --error-exitcode=1 ./"$executeble" $arguments  &> output.txt
+valgrind --tool=helgrind --error-exitcode=1 ./"$executeble" shift 2 "$@" &> /dev/null
 helgrind=$? # Checks whether the helgrind(race check) has succeeded
 if [[ "$helgrind" -gt 0 ]]; then
 threadrace="FAIL"
@@ -42,6 +41,5 @@ echo -e '  Compilation    leaks‬‬ Memory      thread race'
 echo    "     $Compile            $valgrindVar              $threadrace" #program out put
 
 exitnumber="$((2#$secssesfullMake$valgrind$helgrind))" # https://unix.stackexchange.com/questions/65280/binary-to-hexadecimal-and-decimal-in-a-shell-script Binary2Decimal
-rm output.txt
 cd $currentLocation
 exit "$exitnumber"
